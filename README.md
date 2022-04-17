3d72f247aa735e3b58fdb2379237375739aebde6
Byeongji Lim <byeongjilim@gmail.com>
2022-04-17 15:11:52 -0400

    Update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# odin-library
+# Odin Library
+
+- [] Create a skeleton HTML, CSS, JS File
+- [] Create basic form for adding books
+- [] Create constructor and adding functions for books.
+- [] Style the page!
---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = []
+
+function Book(title, author, year, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.read = read;
+}
+
+const addBook = ()
